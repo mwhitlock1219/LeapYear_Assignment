688b620f4ace45e93bc28275aa29bcb610b351b8
Melina Alejos <malejos1219@gmail.com>
2020-06-01 16:50:27 -0400

    final changes

--- LeapYearTest.java
+++ LeapYearTest.java
@@ -1,11 +1,9 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class LeapYearTest {
-
+//Used FizzBuzz as example
     @Test
     public void IsItALeapYear() {
         LeapYear leapYear = new LeapYear();
